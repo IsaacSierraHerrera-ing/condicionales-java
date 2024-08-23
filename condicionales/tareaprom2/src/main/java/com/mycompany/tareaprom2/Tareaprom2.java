3f7cd28e266bf69de44a0d3f91ce2564ff84d583
IsaacSierraHerrera-ing <sierraherreraisaac@gmail.com>
2024-08-23 09:08:34 -0500

    Add files via upload

---
+++ condicionales/tareaprom2/src/main/java/com/mycompany/tareaprom2/Tareaprom2.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tareaprom2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class Tareaprom2 {
+
+    public static void main(String[] args) {
+        
+         Scanner entrada = new Scanner (System.in);
+         
+         
+         
+         System.out.println("digite su nombre:");
+         String nombre = entrada.next();
+        
+         System.out.println("digite su sexo masculino(1) 0 femenino(2):");
+         int sexo = entrada.nextInt();
+         
+         System.out.println("digite su apellido:");
+         String apellido = entrada.next();
+         
+         System.out.println("digite su edad:");
+         int edad = entrada.nextInt();
+         
+         
+         
+         if (edad == 18){
+             System.out.println("usted es mayor de edad");}
+             
+         else {    
+              System.out.println("usted es menor de edad");
+         }
+         
+         if ( sexo == 1 ) {
+              System.out.println("usted es hombre"); }
+         
+         if ( sexo == 2) {
+             
+             System.out.println("usted es mujer");
+         }
+            
+              
+        
+    }
+}
